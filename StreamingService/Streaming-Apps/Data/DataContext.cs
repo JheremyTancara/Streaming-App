4efbd0de63d1e2fc4cd411d2c33dcf3b626ade3d
gabitrujillo <gabrielatrujilloll153@gmail.com>
2025-04-02 11:25:12 -0400

    feat: add partioning

--- StreamingService/Streaming-Apps/Data/DataContext.cs
+++ StreamingService/Streaming-Apps/Data/DataContext.cs
@@ -1,40 +1,50 @@
-using System.Text.Json;
-using Api.Models;
-using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using Api.Models.Interface;
 
-namespace Api.Data
+namespace Api.Models;
+
+[Table("movies")]
+public class Movie : IMovie
 {
-    public class DataContext : DbContext
-    {
-        public DbSet<UserMovie> Users => Set<UserMovie>();
-        public DbSet<Movie> Movies => Set<Movie>();
-        public DbSet<Actor> Actors => Set<Actor>();
-        public DbSet<Director> Directors => Set<Director>();
-
-        public DataContext(DbContextOptions<DataContext> options)
-            : base(options)
-        {
-        }
-
-        public async Task SeedData()
-        {
-            await SeedEntity<Actor>("Data/Files/actors.json", Actors);
-            await SeedEntity<Director>("Data/Files/directors.json", Directors);
-        }
-
-        private async Task SeedEntity<T>(string filePath, DbSet<T> dbSet) where T : class
-        {
-            if (!dbSet.Any())
-            {
-                var jsonData = await File.ReadAllTextAsync(filePath);
-                var entities = JsonSerializer.Deserialize<List<T>>(jsonData);
-
-                if (entities != null && entities.Any())
-                {
-                    await dbSet.AddRangeAsync(entities);
-                    await SaveChangesAsync();
-                }
-            }
-        }
-    }
+    [Column("movieid")]
+    public int MovieID { get; set; } 
+    
+    [Column("title")]
+    public string Title { get; set; } = string.Empty;    
+    
+    [Column("genre")]
+    public List<Genre> Genre { get; set; } = new(); 
+    
+    [Column("releasedate")]
+    public DateTime ReleaseDate { get; set; }
+    
+    [Column("duration")]
+    public double Duration { get; set; }
+    
+    [Column("rating")]
+    public double Rating { get; set; }
+    
+    [Column("description")]
+    public string Description { get; set; } = string.Empty;
+    
+    [Column("cast")]
+    public string[] Cast { get; set; } = Array.Empty<string>(); 
+    
+    [Column("director")]
+    public string Director { get; set; } = string.Empty;
+    
+    [Column("imageurl")]
+    public string ImageUrl { get; set; } = string.Empty;
+    
+    [Column("trailerurl")]
+    public string TrailerUrl { get; set; } = string.Empty;
+    
+    [Column("type")]
+    public ContentType Type { get; set; }
+    
+    [Column("views")]
+    public int Views { get; set; }
+    
+    [Column("agerestriction")]
+    public int AgeRestriction { get; set; }
 }
--- StreamingService/Streaming-Apps/Domain/Models/Movie.cs
+++ StreamingService/Streaming-Apps/Domain/Models/Movie.cs
@@ -1,21 +1,50 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Api.Models.Interface;
 
 namespace Api.Models;
 
+[Table("movies")]
 public class Movie : IMovie
 {
+    [Column("movieid")]
     public int MovieID { get; set; } 
+    
+    [Column("title")]
     public string Title { get; set; } = string.Empty;    
+    
+    [Column("genre")]
     public List<Genre> Genre { get; set; } = new(); 
+    
+    [Column("releasedate")]
     public DateTime ReleaseDate { get; set; }
+    
+    [Column("duration")]
     public double Duration { get; set; }
+    
+    [Column("rating")]
     public double Rating { get; set; }
+    
+    [Column("description")]
     public string Description { get; set; } = string.Empty;
+    
+    [Column("cast")]
     public string[] Cast { get; set; } = Array.Empty<string>(); 
+    
+    [Column("director")]
     public string Director { get; set; } = string.Empty;
+    
+    [Column("imageurl")]
     public string ImageUrl { get; set; } = string.Empty;
+    
+    [Column("trailerurl")]
     public string TrailerUrl { get; set; } = string.Empty;
+    
+    [Column("type")]
     public ContentType Type { get; set; }
+    
+    [Column("views")]
     public int Views { get; set; }
+    
+    [Column("agerestriction")]
     public int AgeRestriction { get; set; }
 }
